01ef7bfb8d9c5029e426f663b5493580b2c31e04
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2024-12-19 10:19:39 +0200

    update

--- profiledef.sh
+++ profiledef.sh
@@ -26,8 +26,6 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
-  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
   ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
   ["/usr/local/bin/fix-keys"]="0:0:755"
 )
